85b11526d163487ac34daf3a9c32b1204d8de598
Emma Faye <emmafaye06@gmail.com>
2023-03-07 14:58:33 +0100

    created fetch request + response promise

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,1 +1,9 @@
-fetch("http://localhost:5678/api/works");
+fetch("http://localhost:5678/api/works")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    data.forEach((works) => {
+      //GENERATE WORKS HERE
+    });
+  });
