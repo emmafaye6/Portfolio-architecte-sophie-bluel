39b17621272db256ebfe97ac0d87a071a7fac5ac
Emma Faye <emmafaye06@gmail.com>
2023-03-08 11:32:09 +0100

    started work elements generation, only images left

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,7 +3,25 @@ fetch("http://localhost:5678/api/works")
     return res.json();
   })
   .then((data) => {
+    console.log(data);
+
+    // FOREACH WILL RUN THROUGH EVERY ELEMENT FROM THE ARRAY
+
     data.forEach((works) => {
-      //GENERATE WORKS HERE
+      // here i wil insert a variable with image paths
+
+      // I CONCATENATE IMAGE FROM WORKS AND HTML TAG AND
+      // TELL THE BROWSER TO ADD IT RIGHT BEFORE THE END OF FIGURE TAG
+
+      const markup = `<figure>${works.imageUrl}</figure>`;
+
+      document
+        .querySelector(".gallery")
+        .insertAdjacentHTML("beforeend", markup);
+
+      const markupCaption = `<figcaption>${works.title}</figcaption>`;
+      document
+        .querySelector(".gallery")
+        .insertAdjacentHTML("beforeend", markupCaption);
     });
   });
